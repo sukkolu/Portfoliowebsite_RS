90ba29f64410b72eacd4b51ebf4c53b53a0300c6
sukkolu <119497504+sukkolu@users.noreply.github.com>
2024-06-12 21:01:39 +0530

    kerjoei

--- src/components/UI/BaseCard.vue
+++ src/components/UI/BaseCard.vue
@@ -14,6 +14,7 @@
         </div>
         <div class="card-buttons">
           <slot name="button-1"></slot>
+
           <slot name="button-2"></slot>
         </div>
       </div>
@@ -24,7 +25,6 @@
 <script>
 import { ref } from 'vue'
 import BaseButton from './BaseButton.vue'
-
 export default {
   components: { BaseButton },
   setup() {
@@ -33,7 +33,6 @@ export default {
     function mouseOnProject() {
       hoverProject.value = true
     }
-
     function mouseOffProject() {
       hoverProject.value = false
     }
@@ -50,7 +49,7 @@ export default {
 <style scoped>
 .card-container {
   width: 75rem;
-  height: 34rem; /* Fixed height for the card */
+  height: 34rem;
   box-shadow: 1px 1px 1.5px 2px rgba(0, 0, 0, 0.3);
   position: relative;
   overflow: hidden;
@@ -85,13 +84,11 @@ export default {
   gap: 2.4rem;
   box-shadow: 0 -2px 5px 2px rgba(0, 0, 0, 0.3);
   padding: 3.2rem 3.2rem 1.2rem 3.2rem;
-  overflow-y: auto; /* Enable vertical scrolling */
 }
 
 .card-content h4 {
   font-size: 3.6rem;
 }
-
 .technologies-logos {
   display: flex;
   gap: 3.2rem;
@@ -115,7 +112,6 @@ export default {
   z-index: 1;
   transform: translateY(0);
 }
-
 @media (max-width: 992px) {
   .card-container {
     width: 65rem;
@@ -144,7 +140,21 @@ export default {
     gap: 2.4rem;
   }
 }
-
+@media (max-width: 768px) {
+  .card-container {
+    width: 52.5rem;
+    height: 23.5rem;
+  }
+  .card-content {
+    font-size: 1.6rem;
+  }
+  .card-content h4 {
+    font-size: 2rem;
+  }
+  .technologies-logos {
+    gap: 2.4rem;
+  }
+}
 @media (max-width: 580px) {
   .card-container {
     width: 45rem;
@@ -161,7 +171,6 @@ export default {
     gap: 2.4rem;
   }
 }
-
 @media (max-width: 480px) {
   .card-container {
     width: 32.5rem;
@@ -179,7 +188,6 @@ export default {
     gap: 2rem;
   }
 }
-
 @media (max-width: 350px) {
   .card-container {
     width: 27rem;
